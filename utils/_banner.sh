7745293ed73782d813487bdeaed995baf67d1d9d
Jailton Nascimento <108580162+jailtonsdn@users.noreply.github.com>
2024-09-30 09:31:50 -0300

    Update _banner.sh

--- utils/_banner.sh
+++ utils/_banner.sh
@@ -23,13 +23,13 @@ print_banner() {
 
 printf "${CYAN_LIGHT}";
 
-printf ${CYAN_LIGHT}"    _____ _____ _   _ ___________ _____ _____  _   _   ___ _____ \n";
-printf ${CYAN_LIGHT}"   |  ___|  _  | | | |_   _| ___ \  ___/  __ \| | | | / _ \_   _| \n";
-printf ${CYAN_LIGHT}"   | |__ | | | | | | | | | | |_/ / |__ | /  \/| |_| |/ /_\ \| |  \n";
-printf ${CYAN_LIGHT}"   |  __|| | | | | | | | | |  __/|  __|| |    |  _  ||  _  || |  \n";
-printf ${CYAN_LIGHT}"   | |___\ \/' / |_| |_| |_| |   | |___| \__/\| | | || | | || |  \n";
-printf ${CYAN_LIGHT}"   \____/ \_/\_\\___/ \___/\_|   \____/ \____/\_| |_/\_| |_/\_/  \n";
- 
+printf ${CYAN_LIGHT}" ######    ####    ##  ##    ####    ##  ##     ##     ######  \n";
+printf ${CYAN_LIGHT}"   ##     ##  ##   ### ##   ##  ##   ##  ##    ####      ##    \n";
+printf ${CYAN_LIGHT}"   ##     ##  ##   ######   ##       ##  ##   ##  ##     ##    \n";
+printf ${CYAN_LIGHT}"   ##     ##  ##   ######   ##       ######   ######     ##    \n";
+printf ${CYAN_LIGHT}"   ##     ##  ##   ## ###   ##       ##  ##   ##  ##     ##    \n";
+printf ${CYAN_LIGHT}"   ##     ##  ##   ##  ##   ##  ##   ##  ##   ##  ##     ##    \n";
+printf ${CYAN_LIGHT}"   ##      ####    ##  ##    ####    ##  ##   ##  ##     ##    \n";
 
 printf "${CYAN_LIGHT}";
   
